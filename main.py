d89ac092cb41827be040ff2581e0861db0088487
oppsec <opps3c@gmail.com>
2022-07-15 15:00:52 -0300

    dev: minor changes x2

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def initializer() -> None:
     parser = ArgumentParser(description="Arbimz Help Module")
     parser.add_argument('--url', type=str, metavar="u", help="Argument used to pass target URL")
     parser.add_argument('--cmd', type=str, metavar="c", default="whoami", help="Argument used to indicate command to execute in target | default: whoami")
-    # parser.add_argument('--file', type=str, metavar='f', help="Argument used to indicate a file with a URLs list")
     parser.add_argument('--kc', type=str, metavar="kc", help="Argument to pass if you already know the Zimbra credentials (admin:admin)")
     args = parser.parse_args()
 
     host_is_alive(args)
+
 
 if __name__ == "__main__":
     return_banner()
